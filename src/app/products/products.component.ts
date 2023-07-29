c5a7ed72d7737d2ca685ebd7023d4af3496cd548
Ajay <ajayzyan100@gmail.com>
2023-07-29 12:34:59 +0530

    fav icon added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
   isClicked: boolean = false;
   addToFav($event: Event) {
     $event.preventDefault();
-    
+    this.isClicked = !this.isClicked
   }
 }
